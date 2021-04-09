5a3d9027336b370ee57f05b5d43f16df8187c4fd
Blinc <65546512+Blinc13@users.noreply.github.com>
2021-04-09 14:16:48 +0300

    Add files via upload

---
+++ Souls.hpp
@@ -0,0 +1,9 @@
+#include "include/Souls.cpp"
+
+enum Act;
+enum drivingSides;
+
+
+class Soul;
+
+class Hero;
---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+#include "View.hpp"
+#include "Control.hpp"
+#include "Render.hpp"
+#include "Physics.hpp"
+#include "Souls.hpp"
+#include "Game.hpp"
+using namespace std;
+
+
+int main()
+{
+  cout<<"hello world"<<endl;
+  int x=Up;
+  cout<<x<<endl;
+  return 0;
+}
